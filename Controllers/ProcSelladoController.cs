10abfceeec7f482080b9553c981f034ec4e46dd7
Diego Fernandez <diegofe21605@gmail.com>
2022-09-23 18:04:09 -0500

    Actualizacion
    Consultas de proceso de sellado

--- Controllers/ProcSelladoController.cs
+++ Controllers/ProcSelladoController.cs
@@ -44,30 +44,73 @@ namespace BagproWebAPI.Controllers
             return Ok(procExtrusion);
         }
 
+        [HttpGet("RollosOT/{ot}")]
+        public ActionResult Get(string ot)
+        {
+            var con = from sel in _context.Set<ProcSellado>()
+                      from cli in _context.Set<Cliente>()
+                      where sel.Ot == ot
+                            && cli.CodBagpro == sel.CodCliente
+                      select new
+                      {
+                          sel.Ot,
+                          sel.Item,
+                          cli.IdentNro,
+                          cli.NombreComercial,
+                          sel.Referencia,
+                          sel.NomReferencia,
+                          sel.Qty,
+                          sel.Unidad,
+                          sel.NomStatus
+                      };
+            return Ok(con);
+        }
+
         [HttpGet("Fechas/{fechaini}/{fechafin}")]
         public ActionResult<ProcSellado> GetOT(DateTime fechaini, DateTime fechafin)
         {
-            var procExtrusion = _context.ProcSellados.Where(x => x.FechaEntrada >= fechaini && x.FechaEntrada <= fechafin).ToList();
-
-            if (procExtrusion == null)
-            {
-                return NotFound();
-            }
-
-            return Ok(procExtrusion);
+            var con = from sel in _context.Set<ProcSellado>()
+                      from cli in _context.Set<Cliente>()
+                      where sel.FechaEntrada >= fechaini
+                            && sel.FechaEntrada <= fechafin
+                            && cli.CodBagpro == sel.CodCliente
+                      select new
+                      {
+                          sel.Ot,
+                          sel.Item,
+                          cli.IdentNro,
+                          cli.NombreComercial,
+                          sel.Referencia,
+                          sel.NomReferencia,
+                          sel.Qty,
+                          sel.Unidad,
+                          sel.NomStatus
+                      };
+            return Ok(con);
         }
 
         [HttpGet("FechasOT/{fechaini}/{fechafin}/{ot}")]
         public ActionResult<ProcSellado> GetOT(DateTime fechaini, DateTime fechafin, string ot)
         {
-            var procExtrusion = _context.ProcSellados.Where(x => x.FechaEntrada >= fechaini && x.FechaEntrada <= fechafin && x.Ot == ot).ToList();
-
-            if (procExtrusion == null)
-            {
-                return NotFound();
-            }
-
-            return Ok(procExtrusion);
+            var con = from sel in _context.Set<ProcSellado>()
+                      from cli in _context.Set<Cliente>()
+                      where sel.Ot == ot
+                            && sel.FechaEntrada >= fechaini
+                            && sel.FechaEntrada <= fechafin
+                            && cli.CodBagpro == sel.CodCliente
+                      select new
+                      {
+                          sel.Ot,
+                          sel.Item,
+                          cli.IdentNro,
+                          cli.NombreComercial,
+                          sel.Referencia,
+                          sel.NomReferencia,
+                          sel.Qty,
+                          sel.Unidad,
+                          sel.NomStatus
+                      };
+            return Ok(con);
         }
 
         [HttpGet("FechaFinOT/{ot}")]
@@ -96,7 +139,6 @@ namespace BagproWebAPI.Controllers
             }
 
         }
-
 
         [HttpGet("OtConSellado/{ot}")]
         public ActionResult<ProcSellado> GetOTSellada(string ot)
