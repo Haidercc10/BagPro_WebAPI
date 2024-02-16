be7440ee971c2f8e96964ba7c9c850f1b1741acd
Diego Fernandez <diegofe21605@gmail.com>
2024-02-16 10:56:03 -0500

    Actualización Nomina Sellado.
    Corrección de la consulta que devuelve la nomina de sellado.

--- obj/Debug/net7.0/BagproWebAPI.AssemblyInfo.cs
+++ obj/Debug/net7.0/BagproWebAPI.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BagproWebAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+30c9ec3df3865a7bbf2ec120ca9f0cf71813ad5e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1a91b97d1b401e361905714858feab5d4029327c")]
 [assembly: System.Reflection.AssemblyProductAttribute("BagproWebAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BagproWebAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
