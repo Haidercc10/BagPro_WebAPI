e54ed2f54633b1d0c515010df9a57836940e5cbc
Háider Cantillo Colón <73559575+Haidercc10@users.noreply.github.com>
2025-10-30 08:58:26 -0500

    Cambios

--- obj/Debug/net7.0/BagproWebAPI.AssemblyInfo.cs
+++ obj/Debug/net7.0/BagproWebAPI.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BagproWebAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+60f05ea795febfa8f4391f857c772b955177ddc6")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a3961818fe2ad5b68e8f7ebfae903c500a32f8a8")]
 [assembly: System.Reflection.AssemblyProductAttribute("BagproWebAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BagproWebAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
